082adb8c048e07e17dad0398fa0764d4b8704460
Netiquette <63346492+sabilimaulana@users.noreply.github.com>
2025-04-15 16:58:30 +0700

    Update wp-config.php

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -38,6 +38,8 @@ define('DB_CHARSET', 'utf8');
 /** The database collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 
+$_SERVER['HTTPS'] = 'on';
+
 /**#@+
  * Authentication unique keys and salts.
  *
